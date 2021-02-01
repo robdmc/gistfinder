d42cd5e562ac9cbbbaa7e88ce286032aebaf4938
Rob deCarvalho <robdmc@gmail.com>
2021-01-31 21:35:51 -0500

    fixed sync bug

--- gistfinder/config.py
+++ gistfinder/config.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import dataset
 import json
 from .utils import cached_property
@@ -17,19 +18,49 @@ class Config:
     LIST_TABLE = 'list_table'
     CODE_TABLE = 'code_table'
 
+    def _check_okay(self):
+        has_user = False
+        has_token = False
+        has_auth_file = os.path.isfile(self.auth_file)
+
+        if has_auth_file:
+            with open(self.auth_file) as buff:
+                blob = json.load(buff)
+                if blob.get('GIST_USER'):
+                    has_user = True
+                if blob.get('GIST_TOKEN'):
+                    has_token = True
+        
+        msg = ''
+        if not has_user:
+            msg += '\nYou need to set up a user: gistfinder -u <username>\n'
+
+        if not has_token:
+            msg += '\nYou need to set up a token: gistfinder -t <access_token>'
+
+        if not has_user and not has_token:
+            msg = '\nYou need to set up a user and token: gistfinder -u <username> -t <access_token>'
+
+        if msg:
+            print(msg, file=sys.stderr)
+            print('\n', file=sys.stdout)
+            sys.exit(1)
+
+        
+
     @cached_property
     def user_url(self):
+        self._check_okay()
+
         with open(self.auth_file) as f:
             user = json.loads(f.read()).get('GIST_USER')
-
-        if not user:
-            raise ValueError('You need to set up a github user')
 
         url = f'https://api.github.com/users/{user}/gists'
         return url
 
     @cached_property
     def db(self):
+        self._check_okay()
         return dataset.connect(url=self.db_url)
 
     @property
@@ -42,11 +73,11 @@ class Config:
 
     @cached_property
     def github_token(self):
+        self._check_okay()
+
         with open(self.auth_file) as f:
             access_token = json.loads(f.read()).get('GIST_TOKEN')
 
-        if not access_token:
-            raise ValueError('You need to set up a github access token')
         return access_token
 
     def update(self, **updates):
